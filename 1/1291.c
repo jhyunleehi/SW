dc4442b7234f1900fc294394d28df61bbea93580
jhyunleehi <jhyunleehi@naver.com>
2018-07-29 09:14:13 +0900

    Update 1291.c
    주석을 처리해야 할 부분을 표시함

--- 1/1291.c
+++ 1/1291.c
@@ -16,7 +16,8 @@ int main() {
     int S, E;
     int K = 0;
     int R = 0;
-    //freopen("data.txt", "r", stdin);
+    freopen("data.txt", "r", stdin);
+    //이것은 주석으로 처리해야 
      
     while (-1 != scanf("%d %d", &S, &E)) {
         if ((S < 2 || S>9) || (E < 2 || E>9)) {
