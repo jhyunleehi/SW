7b201c162f661dcb31fd3257ccc4fe50a2410821
jhyunleehi <jhyunleehi@naver.com>
2018-07-30 01:15:45 +0900

    Create 1337.c
    모든 위치를 모두 계산하는 것 보다는 직전의 위치를 기록하고, 중간값을 기록하고 그 중간값에서 변량을 확인하는 방법을 적용해야 한다.
    그리고 좀더 단순하고 명확하고 혼동이 없는 코드를 위해서...

---
+++ 1/1337.c
@@ -0,0 +1,51 @@
+/**************************************************************
+    Problem: 1337
+    User: jhyunlee
+    Language: C++
+    Result: Success
+    Time:1 ms
+    Memory:1092 kb
+****************************************************************/
+ 
+ 
+#include<stdio.h>
+#include<malloc.h>
+ 
+int main() {
+    int N;
+    int map[100+1][100+1];
+    //freopen("DATA.txt", "r", stdin);
+    scanf("%d", &N);    
+    int x1=1, x2=N, y1=1, y2=N;
+    int num=-1;
+    int x = x1, y = y2;
+    for (int i = N; i >= 1; i--) for (int j = 1; j <= N; j++) map[i][j] = 0;  
+     
+    while (map[y][x] == 0) {
+        x = x1, y = y2;
+        while( (x<=x2)&&(y>=y1)){     
+            map[y--][x++] = (num >= 9)? num = 0: ++num;
+        }
+        x2--; y2--;
+        x = x2; y = y1;
+        while ((x>=x1)&&(y==y1)){            
+            map[y][x--] = (num >= 9) ? num = 0 : ++num;
+        }
+        y1++; x2--;
+        x = x1; y = y1;
+        while((x==x1)&&(y<=y2)){         
+            map[y++][x] = (num >= 9) ? num = 0 : ++num;
+        }
+        x1++;
+        y2--;
+        x = x1; y = y2;
+    }
+ 
+    for (int i = N; i >= 1; i--) {
+        for (int j = 1; j <= N-i+1; j++) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    return 1;
+}
