794fad1a0829425d4b9be8fddf22a97441063d68
jhyunleehi <jhyunleehi@naver.com>
2018-09-22 01:18:50 +0900

    Update 1889.c
    얼마나 단순하게 구현하는 가 하는 것이 핵심이다. 그렇게 하기 위해서는 함수적 관계를 정의하는 훈련이 되어 있어야 한다.

--- 2/1889.c
+++ 2/1889.c
@@ -2,62 +2,38 @@
     Problem: 1889
     User: jhyunlee
     Language: C++
-    Result: Time Limit Exceed
+    Result: Success
+    Time:300 ms
+    Memory:1092 kb
 ****************************************************************/
- 
- 
+  
 #include <stdio.h>
 #include<malloc.h>
  
 static int N = 0;
 static int ans = 0;
 static int Q[14 + 2] = { 0, };
-static int visited[14 + 2] = { 0, };
+static int inc[30], dec[30];
  
-bool isPossible( int row, int col ) {
-    //printf("(%d %d)\n", row, col );
-    if (row == 1) return true;
-    //for (int r = 1; r < row; r++) {
-        //printf("[%d]", Q[r]);
-        //if (Q[r] == Q[row]) return false;
-    //}
-    for (int r = 1; r < row; r++) {
-        //printf("[%d]", Q[r]);     
-        int a = Q[r] - col;
-        int b = r - row;
-        if (a < 0) a = a * -1;
-        if (b < 0) b = b * -1;
-        if (a==b) return false;     
+void backtrace(int r) { 
+    if (r > N) {        
+        ans++;
+        return;
     }
-    return true;
-}
- 
-void backtrace(int row) {
-    //printf("backtrace -- %d\n", row);
-    if (row == N+1) {
-        //printf("*********(%d)*[%d]*******\n", ans,N);
-        ++ans;
-    }
-    else {
-        for (int col = 1; col <= N; col++) {
-            Q[row] = col;
-            if (isPossible(row, col) && visited[col]==0) {
-                visited[col] = 1;
-                backtrace(row + 1);                 
-                visited[col] = 0;
-            }
-            else Q[row] = 0;            
+    for (int i = 1; i <= N; i++) {
+        if(!Q[i]&&!inc[r+i]&&!dec[N+(r-i)+1]){
+            Q[i] = inc[r + i] = dec[N + (r - i) + 1] = 1;
+            backtrace(r + 1);
+            Q[i] = inc[r + i] = dec[N + (r - i) + 1] = 0;
         }
-    }
-    Q[row] = 0; 
+    }   
 }
- 
- 
- 
+  
  
 int main() {    
     scanf("%d", &N);        
-    ans = 0;
     backtrace(1);
     printf("%d\n", ans);
+ 
+    return 0;
 }
