18e34614a23e7360664d4030686d2b395e6c6401
jhyunleehi <jhyunleehi@naver.com>
2018-09-22 01:16:59 +0900

    Update 1889.c
    N*N*N 만큼 연산하게 되는 것을 중간에 어떻게 줄여 줄수 있는가  그것이 문제군.. 길고 긴 각각의 컬럼 을 어떤 산술적 합에 의한 대표값을 정의하고 그것이 부합 한다면 다시 연산 하지 않도록 간결하고 단순화 시키는 것이 핵심이군...

--- 2/1889.c
+++ 2/1889.c
@@ -2,9 +2,7 @@
     Problem: 1889
     User: jhyunlee
     Language: C++
-    Result: Success
-    Time:300 ms
-    Memory:1092 kb
+    Result: Time Limit Exceed
 ****************************************************************/
  
  
@@ -14,12 +12,15 @@
 static int N = 0;
 static int ans = 0;
 static int Q[14 + 2] = { 0, };
-static int inc[30], dec[30];
 static int visited[14 + 2] = { 0, };
  
 bool isPossible( int row, int col ) {
     //printf("(%d %d)\n", row, col );
-    if (row == 1) return true;  
+    if (row == 1) return true;
+    //for (int r = 1; r < row; r++) {
+        //printf("[%d]", Q[r]);
+        //if (Q[r] == Q[row]) return false;
+    //}
     for (int r = 1; r < row; r++) {
         //printf("[%d]", Q[r]);     
         int a = Q[r] - col;
@@ -31,35 +32,32 @@ bool isPossible( int row, int col ) {
     return true;
 }
  
-void backtrace(int r) {
+void backtrace(int row) {
     //printf("backtrace -- %d\n", row);
-    if (r > N) {
+    if (row == N+1) {
         //printf("*********(%d)*[%d]*******\n", ans,N);
-        ans++;
-        return;
+        ++ans;
     }
-    for (int i = 1; i <= N; i++) {
-        if(!Q[i]&&!inc[r+i]&&!dec[N+(r-i)+1]){
-            Q[i] = inc[r + i] = dec[N + (r - i) + 1] = 1;
-            backtrace(r + 1);
-            Q[i] = inc[r + i] = dec[N + (r - i) + 1] = 0;
+    else {
+        for (int col = 1; col <= N; col++) {
+            Q[row] = col;
+            if (isPossible(row, col) && visited[col]==0) {
+                visited[col] = 1;
+                backtrace(row + 1);                 
+                visited[col] = 0;
+            }
+            else Q[row] = 0;            
         }
-    }   
+    }
+    Q[row] = 0; 
 }
  
  
  
  
 int main() {    
-    //freopen("data.txt", "r", stdin);
     scanf("%d", &N);        
-    //for(int i=1; i<=14; i++){
-    //  N = i;
-    //  ans = 0;
+    ans = 0;
     backtrace(1);
-    //    printf("%d , %d\n", i, ans);
-    //}
     printf("%d\n", ans);
- 
-    return 0;
 }
