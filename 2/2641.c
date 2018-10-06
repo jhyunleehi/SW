ac3ee61ec3e9180ebc38fd546c06cd99befa3136
jhyunleehi <jhyunleehi@naver.com>
2018-10-06 16:15:54 +0900

    Update 2641.c
    1. 트럭은 한번 방문한 도시를 다시 방문하지 않고 1번 도시부터 출발하기 때문에  1번 도시와 가까운 순으로 박스를 옮기는 것이 최적입니다.
    2. 따라서 도착 도시를 기준으로 오름차순 정렬을 해주고 도착 도시가 같다면 시작 도시를 기준으로 오름차순 정렬을 해줍니다.
    3. 시작도시부터 도착도시까지 가장 많이 적재된 박스 양을 구합니다.
    4. 3번에서 구한 값에 대해 더 적재할 수 있는 양을 파악하고 결과 값에 더해줍니다.
    5. 해당 구간에 4번에 구한 값만큼 더해줍니다.
    6. 3~5번을 M번 반복합니다.

--- 2/2641.c
+++ 2/2641.c
@@ -1,99 +1,119 @@
+/**************************************************************
+    Problem: 2641
+    User: jhyunlee
+    Language: C++
+    Result: Success
+    Time:31 ms
+    Memory:16824 kb
+****************************************************************/
+ 
+ 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<malloc.h>
-
+ 
 typedef struct box {
-	int dist;
-	int from;
-	int to;
-	int load;
+    int dist;
+    int from;
+    int to;
+    int load;
 };
-
+ 
 int V;
 int MAX;
 int N;
 box *B;
-
+ 
 void makeMap(int **M, int N) {
-	int F, T, L;
-	for (int i = 0; i < N; i++) {
-		F = (B + i)->from;
-		T = (B + i)->to;
-		L= (B + i)->load;
-		*(*(M + F) + T) = L;
-	}
+    int F, T, L;
+    for (int i = 0; i < N; i++) {
+        F = (B + i)->from;
+        T = (B + i)->to;
+        L= (B + i)->load;
+        *(*(M + F) + T) = L;
+    }
 }
-
+ 
 void qsort(struct box *Load, int Left, int Right) {
-	if (Left >= Right) return;
-	int p = Left;
-	int q = Right;
-	int v = (Load + ((Left + Right) / 2) )->dist;  //dist=from -to 거리 증가순으로 정렬	
-	while (p <= q) {
-		while ((Load+p)->dist < v) p++;
-		while ((Load+q)->dist > v) q--;
-		if (p <= q) {
-			//swap
-			box temp = *(Load + p);
-			*(Load + p) = *(Load + q);
-			*(Load + q) = temp;
-			p++; q--;
-		}
-	}
-	if (Left < q) qsort(Load, Left, q);
-	if (Right > p) qsort(Load, p, Right);		
+    if (Left >= Right) return;
+    int p = Left;
+    int q = Right;
+    int v = (Load + ((Left + Right) / 2) )->dist;  //dist=from -to 거리 증가순으로 정렬   
+    while (p <= q) {
+        while ((Load+p)->dist < v) p++;
+        while ((Load+q)->dist > v) q--;
+        if (p <= q) {
+            //swap
+            box temp = *(Load + p);
+            *(Load + p) = *(Load + q);
+            *(Load + q) = temp;
+            p++; q--;
+        }
+    }
+    if (Left < q) qsort(Load, Left, q);
+    if (Right > p) qsort(Load, p, Right);        
 }
-
-
+ 
+ 
 int main() {
-	freopen("data.txt", "r", stdin);
-	scanf("%d %d", &V, &MAX);
-	scanf("%d", &N);
-	int **Map = (int**)malloc(sizeof(int*)*(V+1));
-	for (int i = 0; i <= V; i++) {
-		*(Map + i) = (int *)malloc(sizeof(int)*(V+1));
-		for (int j = 0; j <= V; j++)
-			*(*(Map + i) + j) = 0;
-	}
-
-	B = (box*)malloc(sizeof(struct box)*N);
-	
-	for (int i = 0; i < N; i++) {
-		scanf("%d %d %d", &(B + i)->from, &(B + i)->to, &(B + i)->load);
-		(B + i)->dist = (B + i)->to - (B + i)->from;
-	}
-	
-	qsort(B, 0, N - 1);
-	makeMap(Map, N);
-	for (int i = 0; i < N; i++) {
-		printf("[%d][%d][%d][%d] \n", (B + i)->dist, (B + i)->from, (B + i)->to, (B + i)->load);
-	}
-		
-	int ans = 0;	
-	int *W = (int *)malloc(sizeof(int)*(V + 1));//해당위치에서 실려 있는 량
-	for (int i = 0; i <= V; i++) *(W + i) = 0;
-	int F, T, L, D, before_load,optimized_load;
-
-	for (int i = 0; i < N; i++) {
-		F = (B + i)->from;
-		T = (B + i)->to;
-		L = (B + i)->load;
-		before_load = 0;
-		for (int j = F; j < T; j++) {
-			if (before_load < *(W + j)) 
-				before_load = *(W + j);
-		}
-		optimized_load = 0;
-		optimized_load = (MAX - before_load) < (B + i)->load ? (MAX - before_load) : (B + i)->load;
-		printf("(%d) [%d] [%d] [%d] ", i, before_load, optimized_load, ans);
-		for (int k = F; k < T; k++) {
-			*(W + k) += optimized_load;
-		}
-		ans += optimized_load;
-		for (int m = 0; m < V; m++) printf("%2d ", *(W + m));
-		printf("\n");
-	}
-	printf("%d\n", ans);
-
-	return 0;
+    //freopen("data.txt", "r", stdin);
+    scanf("%d %d", &V, &MAX);
+    scanf("%d", &N);
+    int **Map = (int**)malloc(sizeof(int*)*(V+1));
+    for (int i = 0; i <= V; i++) {
+        *(Map + i) = (int *)malloc(sizeof(int)*(V+1));
+        for (int j = 0; j <= V; j++)
+            *(*(Map + i) + j) = 0;
+    }
+ 
+    B = (box*)malloc(sizeof(struct box)*N);
+     
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d %d", &(B + i)->from, &(B + i)->to, &(B + i)->load);
+        (B + i)->dist = ((B + i)->to)*10000+ ((B + i)->from);
+    }
+     
+    qsort(B, 0, N - 1);
+    makeMap(Map, N);
+    /*
+    for (int i = 0; i < N; i++) {
+        printf("[%d][%d][%d][%d] \n", (B + i)->dist, (B + i)->from, (B + i)->to, (B + i)->load);
+    }
+    */
+    /*
+    for (int i = 1; i <= V; i++) {
+        for (int j = 1; j <= V; j++) {
+            printf("%2d ", *(*(Map + i) + j));
+        }
+        printf("\n");
+    }
+    */
+         
+    int ans = 0;    
+    int *W = (int *)malloc(sizeof(int)*(V + 1));//해당위치에서 실려 있는 량
+    for (int i = 0; i <= V; i++) *(W + i) = 0;
+    int F, T, L, D, before_load,optimized_load;
+ 
+    for (int i = 0; i < N; i++) {
+        F = (B + i)->from;
+        T = (B + i)->to;
+        L = (B + i)->load;
+        before_load = 0;
+        for (int j = F; j < T; j++) {
+            if (before_load < *(W + j)) 
+                before_load = *(W + j);
+        }
+        optimized_load = 0;
+        optimized_load = (MAX - before_load) < (B + i)->load ? (MAX - before_load) : (B + i)->load;
+        //printf("(%3d) [%3d] [%3d] [%3d] ", i, before_load, optimized_load, ans);
+        for (int k = F; k < T; k++) {
+            *(W + k) += optimized_load;
+        }
+        ans += optimized_load;
+        //for (int m = 0; m <= V; m++) printf("%2d ", *(W + m));
+        //printf("\n");
+    }
+    printf("%d\n", ans);
+ 
+    return 0;
 }
