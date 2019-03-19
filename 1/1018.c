50042ba7f994e192a4b6a6aa5b251d3b6e31100a
jhyunleehi <jhyunleehi@naver.com>
2019-03-20 00:24:46 +0900

    Update 1018.c

--- 1/1018.c
+++ 1/1018.c
@@ -3,29 +3,47 @@
 #include<malloc.h>
 
 int main() {
-	int min = 0x7FFFFFFF;
+	int MIN = 0x7FFFFFFF;
 	int N[4] = { 0, };
 	int M[4] = { 0, };
+	int List[4] = { 0,0,0,0 };
 	int temp = 0;
 	int cnt = 0;
 	freopen("Text.txt", "r", stdin);
 	scanf("%d %d %d %d", &N[0], &N[1], &N[2], &N[3]);
 	for (int i = 0; i < 4; i++) {
 		temp = (N[i % 4] * 1000) + (N[(i + 1) % 4] * 100) + (N[(i + 2) % 4] * 10) + (N[(i + 3) % 4]);
-		if (temp < min) min = temp;
+		List[i] = temp;
+		if (temp < MIN) MIN = temp;
 		//printf("min %d temp %d \n", min, temp);
 	}
-	int k = 0;
-	for (int i = 1000; i >= 1; i=i/10) { 
-		M[k] = min / i; 
-		min = min - M[k++] * i;
-	}
-	//for (int i = 0; i < 4; i++) {printf("[%d] \n", M[i]);}
 	
-	k = 0;
-	for (int i = 900; i >=1 ; i=i/10) {
-		cnt+=(M[k++] - 1)*i;
+	//for (int i = 0; i < 4; i++) {printf("[%d] \n", List[i]);}
+	
+	
+	for (int i = 1111; i <= MIN; i++) {		
+		cnt++;
+		temp = i;
+		int k = 0;
+		for (int j = 1000; j >= 1; j = j / 10) {			
+			M[k] = temp / j ;
+			temp = temp - M[k++] * j;
+		}		
+		printf("[%d][%d][%d][%d]\n", M[0], M[1], M[2], M[3]);
+		if ((M[0] == 0) || (M[1] == 0) || (M[2] == 0) || (M[3] == 0)) {
+			printf("--[%d][%d][%d][%d]\n", M[0], M[1], M[2], M[3]);
+			cnt--;
+			//printf("%d\n", cnt);
+			continue;
+		}
+		for (int m = 0; m < 4; m++) {
+			if (List[m] == i) {
+				printf("--[%d][%d][%d][%d]\n", M[0], M[1], M[2], M[3]);
+				cnt--;
+				break;
+			}
+		}	
 	}
-	cnt += M[3] - 1;	
-	printf("%d\n", cnt);
+	printf("%d\n", ++cnt);
+	
 }
