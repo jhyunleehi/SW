17cea54100bfd73929483bbaf24f87053133c414
jhyunleehi <jhyunleehi@naver.com>
2019-05-14 00:09:02 +0900

    Update 1053.c
    time out 발생.. ?

--- 1053.c
+++ 1053.c
@@ -2,26 +2,32 @@
 
 #include<stdio.h>
 #include<malloc.h>
-int M[1,000,000,000 + 1] = {0,};
 
+int M[2] = { 0, };
 int call(int n) {
-	printf("call n:%d M:%d\n", n, M[n]);
-	if (M[n] != 0) return M[n];
+	//printf("call %d\n", n );	
 	if (n == 0) return M[0];
 	if (n == 1) return M[1];
-	M[n] = call(n - 1) + call(n - 2);
-	if (M[n] > 1000) M[n] % 1000;
-	return M[n];
+	int temp;
+	for (int i = 2; i <= n; i++) {
+		temp = M[0] + M[1];
+		M[0] = M[1];
+		M[1]= temp;
+		if (M[1] >= 10000) M[1] = M[1] % 10000;
+		//if (M[0] > 10000) M[0] = M[0] % 10000;
+		//printf("[%d] ", M[1]);
+	}		
+	return M[1];
 }
 
 int main() {
 	freopen("data.txt", "r", stdin);
-	int N = 0;
-	M[0] = 0;
-	M[1] = 1;
+	int N = 0;	
 	scanf("%d", &N);
 	while (N != -1) {
-		printf("%d", call(N) );
+		M[0] = 0;
+		M[1] = 1;
+		printf("%d\n", call(N) );
 		scanf("%d", &N);
 	}
 }
