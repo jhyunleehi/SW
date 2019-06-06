855da2bee9552e4ed9bcb9aaba64bbd820254af6
jhyunleehi <jhyunleehi@naver.com>
2019-06-06 09:53:54 +0900

    Update 1510.c

--- 1510.c
+++ 1510.c
@@ -16,7 +16,7 @@ void rcall(int row, int depth) {
 	int cnt = 0;
 	depth++;
 	if (depth > count) {
-		count = depth;		
+		count = depth;
 	}
 	for (int j = 0; j < N; j++)
 		if (map[row][j] != 0) cnt++;
@@ -28,44 +28,59 @@ void rcall(int row, int depth) {
 		}
 	}
 }
+void qsort(struct item *pi, int L, int R) {
+	int p = L;
+	int q = R;
+	int v = (pi+((L + R) / 2))->a;
+	int ta, tb;
+	while (p <= q) {
+		while ((pi + p)->a < v)p++;
+		while ((pi + q)->a > v)q--;
+		if (p <= q) {
+			ta = (pi + p)->a; tb = (pi + p)->b;
+			(pi + p)->a = (pi + q)->a;
+			(pi + p)->b = (pi + q)->b;
+			(pi + q)->a = ta; (pi + q)->b = tb;
+			p++; q--;
+		}
+	}
+	if (q > L)qsort(pi, L, q);
+	if (p < R)qsort(pi, p, R);
+}
+
 int main() {
-	freopen("data.txt", "r", stdin);	
+	freopen("data.txt", "r", stdin);
 	scanf("%d", &N);
 	struct item *pi = (struct item *)malloc(sizeof(struct item)*N);
+	int a, b;
+	int map[100][2] = { 0, };
 	for (int i = 0; i < N; i++) {
-		scanf("%d %d", &(pi+i)->a, &(pi+i)->b);
+		scanf("%d %d", &a, &b);
+		(pi + i)->a = (a >= b) ? a : b;
+		(pi + i)->b = (b > a) ? a : b;
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (i == j) { map[i][j] = 0; continue; }
-			if ((((pi + i)->a >= (pi + j)->a) && ((pi + i)->b >= (pi + j)->b)) ||
-				(((pi + i)->a >= (pi + j)->b) && ((pi + i)->b >= (pi + j)->a)) )				
-				map[i][j] = 1;
-			else
-				map[i][j] = 0;
-		}
-	}
-	int cnt;
-	int max = 0x7fffffff + 1;
-	int st;
-	for (int i = 0; i < N; i++) {
-		cnt = 0;
-		for (int j = 0; j < N; j++) {
-			//printf("[%d] ", map[i][j]);
-			if (map[i][j] == 1) cnt++;
-			if (cnt > max) { max=cnt; st = i; }
-		}
-		//printf("\n");
-	}
-	cnt = 0;
-	rcall(st, 0);
+	qsort(pi, 0, N-1);
+	for (int i = 0; i < N; i++)	printf("#%2d %3d %3d \n", i, (pi + i)->a, (pi + i)->b);
 
-	for (int i = 0; i < N; i++) {	
-		for (int j = 0; j < N; j++) {
-			printf("[%d] ", map[i][j]);	
+	for (int i = 1; i < N; i++) {
+		for (int j = i; j >= 0; j--) {
+			if ((pi + i)->b >= (pi + j)->b)
+				map[i][0] ++;
 		}
-		printf("\n");
+	}
+	int cnt = 0;
+	//map[0][0] = 1;	
+	for (int i = 1; i < N; i++) {		
+		for (int j = i; j >= 0; j--) {
+			if ((pi + i)->b >= (pi + j)->b)
+				if(map[i][1] <= map[j][1] + 1)
+					map[i][1] = map[j][1] + 1;
+			if (cnt < map[i][1]) cnt = map[i][1];
+		}		
 	}
 
-	printf("%d\n", count);
+	for (int i = 0; i < N; i++)	printf("#%2d [%3d] [%3d]\n", i, map[i][0], map[i][1]); 
+
+
+	printf("%d\n", cnt+1);
 }
