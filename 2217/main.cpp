a34ba128a5ac31500e50fafe7eb0edba1ed8cae4
jhyunleehi <jhyunleehi@naver.com>
2021-02-05 10:20:07 +0900

    2217

--- 2217/main.cpp
+++ 2217/main.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
-int a;
+int n,m[100001],k,cnt;
+long long max = 0;
 int main() {
-	scanf("%d",&a);
-	if (a % 2) printf("SK\n");
-	else  printf("CY\n");
+	freopen("data.txt", "r", stdin);
+	scanf("%d",&n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &k);
+		m[k]++;
+	}	
+	for (int i = 100000; i >= 1; i--) {
+		if (m[i] != 0) {
+			cnt+=m[i];
+			max = (max < (i * cnt)) ? (i * cnt) : max;
+		}		
+	}
+	printf("%lld\n", max);
 	return 0;
 }
