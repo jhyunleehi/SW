87fc64603b48415a7793b1856f6b5bea32ea19b6
jhyunleehi <jhyunleehi@naver.com>
2023-05-08 23:48:21 +0900

    2850

---
+++ BJ/2850/2850.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+char str[15];
+int ans;
+int main() {
+	freopen("data.txt", "r", stdin);
+	for (int i = 1; i <= 5; ++i) {
+		fgets(str, sizeof(str), stdin);				
+		for (int j = 0; str[j]; ++j) {
+			if ((str[j] == 'F') && (str[j + 1] == 'B') && (str[j + 2] == 'I')) {
+				printf("%d ", i);
+				ans++;
+				break;
+			}
+		}
+	}
+	if (ans == 0) {
+		printf("HE GOT AWAY!\n");
+	}
+}
