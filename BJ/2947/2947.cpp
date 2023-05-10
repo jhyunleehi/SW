d171ddca11da00ba5306808fed57bafd0aa4aa10
jhyunleehi <jhyunleehi@naver.com>
2023-05-10 23:37:00 +0900

    ok

---
+++ BJ/2947/2947.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdio>
+int N[6];
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    freopen("data.txt", "r", stdin);
+    for (int i = 1; i <= 5; ++i)   scanf("%d", N + i);
+    for (; !((N[1] == 1) && (N[2] == 2) && (N[3] == 3) && (N[4] == 4) && (N[5] == 5));) {
+        for (int i = 1; i < 5; ++i) {
+            if (N[i] > N[i + 1]) {
+                swap(&N[i], &N[i + 1]);
+                for (int j = 1; j <= 5; ++j) printf("%d ", N[j]);
+                printf("\n");
+            }
+        }
+    } 
+}
---
+++ BJ/3035/3035.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int R, C, ZR, ZC;
+char S[51][51];
+char M[255][255];
+int main()
+{
+    freopen("data.txt", "r", stdin);
+    scanf("%d %d %d %d", &R,&C,&ZR,&ZC);
+    for (int i = 0; i < R; i++) {
+        scanf("%s", S[i]);
+    }
+    for (int r = 0; r < R; ++r) {
+        for (int c = 0; c < C; ++c) {
+            for (int y = 0; y < ZR; ++y) {
+                for (int x = 0; x < ZC; ++x) {
+                    M[r*ZR + y][c*ZC + x] = S[r][c];
+                }
+            }
+        }
+    }
+    for (int r = 0; r < R*ZR; ++r) {
+        for (int c = 0; c < C*ZC; ++c) {
+            printf("%c", M[r][c]);
+        }
+        printf("\n");
+    }
+}
