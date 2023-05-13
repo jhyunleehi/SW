949928384eaab16fc2bb8cfb7bf1940f6e894fe6
jhyunleehi <jhyunleehi@naver.com>
2023-05-13 22:37:04 +0900

    2023-0513

---
+++ BJ/5613/5613.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+long long ans;
+int n;
+char s[9];//100,000,000
+int main()
+{
+    freopen("data.txt", "r", stdin);
+    scanf("%d", &n);
+    ans += n;
+    for (;;) {
+        scanf("%s", s);
+        if (s[0] == '=') {
+            printf("%lld\n", ans);
+            return 0;
+        }
+        switch (s[0]) {
+        case '+':
+            scanf("%d", &n);
+            ans += n;
+            break;
+        case '-':
+            scanf("%d", &n);
+            ans -= n;
+            break;
+        case '*':
+            scanf("%d", &n);
+            ans *= n;
+            break;
+        case '/':
+            scanf("%d", &n);
+            ans /= n;
+           break;        
+        }
+    }
+}
---
+++ BJ/9094/9094.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int N, M, T;
+int main()
+{
+    freopen("data.txt", "r", stdin);
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++) {
+        int count = 0;
+        scanf("%d %d", &N, &M);
+        for (int b = 2; b < N; b++) {
+        for (int a = 1; a < b; a++) {
+            if ((a * a + b * b + M) % (a * b) == 0) {
+                printf("%d %d\n", a, b);
+                count++;
+            }
+        }
+        }
+        printf("%d\n", count);
+    }
+}
